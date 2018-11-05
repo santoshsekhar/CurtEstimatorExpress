da3ba5b05358f7b875ccba4d15b183cc0ad64871
mwoolery <mwoolery@nwmissouri.edu>
2018-11-04 19:48:43 -0600

    trying to fix some problems

--- app.js
+++ app.js
@@ -11,23 +11,23 @@
 // Module dependencies
 const express = require('express')
 const http = require('http')
-const expressLayouts = require('express-ejs-layouts')
-const favicon = require('serve-favicon')
+//const expressLayouts = require('express-ejs-layouts')
+//const favicon = require('serve-favicon')
 const path = require('path')
 const bodyParser = require('body-parser')
-const engines = require('consolidate')
-const session = require('express-session')
-const errorHandler = require('errorhandler')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const expressValidator = require('express-validator')
-const expressStatusMonitor = require('express-status-monitor')
-const LOG = require('./utils/logger.js')
+//const engines = require('consolidate')
+//const session = require('express-session')
+//const errorHandler = require('errorhandler')
+//const dotenv = require('dotenv')
+//const mongoose = require('mongoose')
+//const expressValidator = require('express-validator')
+//const expressStatusMonitor = require('express-status-monitor')
+//const LOG = require('./utils/logger.js')
 
 // Load environment variables from .env file, where API keys and passwords are configured.
 // dotenv.load({ path: '.env.example' })
-dotenv.load({ path: '.env' })
-LOG.info('Environment variables loaded.')
+//dotenv.load({ path: '.env' })
+//LOG.info('Environment variables loaded.')
 
 // app variables
 const DEFAULT_PORT = 3000
@@ -43,7 +43,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 // specify desired view engine
 app.set('view engine', 'ejs')
-app.engine('ejs', engines.ejs)
+//app.engine('ejs', engines.ejs)
 
 // configure middleware.....................................................
 // app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
@@ -60,7 +60,7 @@ app.engine('ejs', engines.ejs)
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(expressValidator())
 // app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
-app.use(expressLayouts)
+//app.use(expressLayouts)
 // app.use(errorHandler()) // load error handler
 
 const routes = require('./routes/index.js')
--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,14 @@
  */
 
 const express = require('express')
-const LOG = require('../utils/logger.js')
 
-LOG.debug('START routing')
+
+
 const router = express.Router()
 
 // Manage top-level request first
 router.get('/', (req, res, next) => {
-  LOG.debug('Request to /')
+
   res.render('index.ejs', { title: 'Express App' })
 })
 
@@ -23,5 +23,5 @@ router.get('/', (req, res, next) => {
 router.use('/about', require('../controllers/about.js'))
 // router.use('/puppy', require('../controllers/puppy.js'))
 
-LOG.debug('END routing')
+
 module.exports = router
