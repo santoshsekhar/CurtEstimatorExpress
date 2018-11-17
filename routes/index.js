f03c1c51c60049a2c080e15f2ded8fe31e1b28e2
SANTOSH SEKHAR M B <santosh@SANTOSHs-MacBook-Pro.local>
2018-11-17 13:42:34 -0600

    added register tab and page to register admin

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.get('/', (req, res, next) => {
 // Defer path requests to a particular controller
 router.use('/about', require('../controllers/about.js'))
 router.use('/coating',require('../controllers/coating.js'))
+router.use('/register',require('../controllers/register.js'))
 
 
  
