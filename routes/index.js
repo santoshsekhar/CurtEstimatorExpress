46bbea53e7f62951885724413fb40036ea939807
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2018-11-18 20:10:02 -0600

    Add the contact file to get the contact details

--- routes/index.js
+++ routes/index.js
@@ -19,11 +19,18 @@ router.get('/', (req, res, next) => {
   res.render('index.ejs', { title: 'Express App' })
 })
 
+router.get('/contact', (request, response, next) => {
+
+  response.render('contact.ejs')
+})
+
 // Defer path requests to a particular controller
 router.use('/about', require('../controllers/about.js'))
 router.use('/coating',require('../controllers/coating.js'))
 router.use('/account',require('../controllers/user.js'))
 router.use('/register',require('../controllers/register.js'))
+
+
 
 
 
