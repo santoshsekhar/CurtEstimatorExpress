c71118efd3cb42c75fcb22e24d71e4e59133112f
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2018-12-02 21:32:01 -0600

    Added about.js in the controllers and few ejs files for team members

---
+++ controllers/about.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const api = express.Router()
+// Specify the handler for each required combination of URI and HTTP verb
+
+// HANDLE VIEW DISPLAY REQUESTS --------------------------------------------
+
+// GET t1
+api.get('/t1', (req, res) => {
+    console.log(`Handling GET ${req}`)
+    res.render('about/t1/index.ejs' 
+  
+    )
+  })
+  api.get('/t1/a', (req, res) => {
+    console.log(`Handling GET ${req}`)
+    res.render('about/t1/a/index.ejs'
+    
+     )
+  })
+  api.get('/t1/b', (req, res) => {
+    console.log(`Handling GET ${req}`)
+    res.render('about/t1/b/index.ejs' 
+   
+    )
+  })
+  
+  api.get('/t1/c', (req, res) => {
+    console.log(`Handling GET ${req}`)
+    res.render('about/t1/c/index.ejs' 
+   
+    )
+  })
+  
+  api.get('/t1/d', (req, res) => {
+    console.log(`Handling GET ${req}`)
+    res.render('about/t1/d/index.ejs' 
+   
+    )
+  })
+  module.exports = api
+  
+  
+  about/t1/index.ejs
+  
+  <br/>
+  <br/>
+  <br/>
+  <h1> EJS Team </h1>
+  <h2>Software Developers</h2>
+  
+  
+  <br/>
+  <br/>
+  
+  
+  
