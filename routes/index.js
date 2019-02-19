0f47f852cc2c72a378b79e0fb0c0a5fc5b40d1d0
Yashwanth Reddy Bommineni <S531519@nwmissouri.edu>
2019-02-19 12:50:18 -0600

    integrated mailgun api

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,49 @@ router.get('/contact',  (req, res) =>{
       })    
     })
 
+router.post('/contact', (req, res) => {
+    
+    var api_key = 'c0b7e10cd4dfb790a98d671d55454bdb-9ce9335e-fedad93a';
+var domain = 'sandbox035d72cee28e4d1dacab0a8ee9079584.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+ 
+var data = {
+  from: 'Mailer <s531519@nwmissouri.edu>',
+  to: 'yashwanth.reddyb94@gmail.com',
+  subject: req.body.firstname + ' ' + req.body.lastname,
+  html: `<html><h1>
+  contact Details
+</h1>
+
+<table>
+  <tr>
+      <td>FirstName: </td>
+      <td>${req.body.firstname}</td>
+  </tr>
+  <tr>
+      <td>Last Name:</td>
+      <td>${req.body.lastname}</td>
+  </tr>
+  <tr>
+      <td>Date of Birth</td>
+      <td>${req.body.dob}</td>
+  </tr>
+  <tr>
+      <td>Email</td>
+      <td>${req.body.email}</td>
+  </tr>
+  <tr>
+      <td>Description</td>
+      <td>${req.body.description}</td>
+  </tr>
+</table> </html>`
+};
+ 
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});
+res.redirect('/contact')
+})
 router.use('/coating',require('../controllers/coating.js'))   
  
     // router.get('/coating',(req,res)=>{
