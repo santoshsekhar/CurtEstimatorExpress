00dcd153000950bad983e5169f9a7c88bcb1f640
SANTOSH SEKHAR <35478448+santoshsekhar@users.noreply.github.com>
2019-04-30 18:09:17 -0500

    Updated contact.java

--- seleniumTests/EstimatorTest/src/contact.java
+++ seleniumTests/EstimatorTest/src/contact.java
@@ -6,13 +6,18 @@ public class contact {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		// To include the web driver for chrome browser
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\s531503\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
+		// opens the website 
 		driver.get("https://curtestimator.herokuapp.com/");
+		//to maximize the window on opening
 		driver.manage().window().maximize();
 
 		// Contact page - PASS
 		driver.get("https://curtestimator.herokuapp.com/contact");
+		// waits 2000 milli seconds for next action
 		Thread.sleep(2000);
 		
 		// Sending email without any sender information- FAIL
@@ -42,6 +47,7 @@ public class contact {
 		driver.findElement(By.xpath("/html/body/div/div/div[1]/div[2]/form/div/button")).click();
 		Thread.sleep(3000);
 		
+		// closes the web driver
 		driver.close();
 
 		
